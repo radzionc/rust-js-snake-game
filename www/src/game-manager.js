aad8de4ccf38e72149b6c1035058f65cd9b08068
Rodion Chachura <geekrodion@gmail.com>
2019-12-05 06:47:24 +0300

    feature: controller class

--- www/src/game-manager.js
+++ www/src/game-manager.js
@@ -1,16 +1,9 @@
-import { Vector, Game, Movement } from "wasm-snake-game";
+import { Vector, Game } from "wasm-snake-game";
+
 import { View } from './view'
+import { Controller } from './controller'
 
 const UPDATE_EVERY = 1000 / 60
-
-const MOVEMENT_KEYS = {
-  [Movement.TOP]: [87, 38],
-  [Movement.RIGHT]: [68, 39],
-  [Movement.DOWN]: [83, 40],
-  [Movement.LEFT]: [65, 37]
-}
-
-const STOP_KEY = 32
 
 export class GameManager {
   constructor() {
@@ -21,6 +14,19 @@ export class GameManager {
       this.game.height,
       this.render.bind(this)
     )
+    this.controller = new Controller(
+      this.onStop.bind(this)
+    )
+  }
+
+  onStop() {
+    const now = Date.now()
+    if (this.stopTime) {
+      this.stopTime = undefined
+      this.lastUpdate = this.time + now - this.lastUpdate
+    } else {
+      this.stopTime = now
+    }
   }
 
   restart() {
@@ -33,7 +39,6 @@ export class GameManager {
     )
     this.lastUpdate = undefined
     this.stopTime = undefined
-    this.movement = undefined
   }
 
   render() {
@@ -49,7 +54,7 @@ export class GameManager {
     if (!this.stopTime) {
       const lastUpdate = Date.now()
       if (this.lastUpdate) {
-        this.game.process(lastUpdate - this.lastUpdate, this.movement)
+        this.game.process(lastUpdate - this.lastUpdate, this.controller.movement)
         if (this.game.is_over()) {
           this.restart()
           return
@@ -65,21 +70,6 @@ export class GameManager {
   }
 
   run() {
-    window.addEventListener('keydown', ({ which }) => {
-      this.movement = Object.keys(MOVEMENT_KEYS).find(key => MOVEMENT_KEYS[key].includes(which))
-    })
-    window.addEventListener('keyup', ({ which }) => {
-      this.movement = undefined
-      if (which === STOP_KEY) {
-        const now = Date.now()
-        if (this.stopTime) {
-          this.stopTime = undefined
-          this.lastUpdate = this.time + now - this.lastUpdate
-        } else {
-          this.stopTime = now
-        }
-      }
-    })
     setInterval(this.tick.bind(this), UPDATE_EVERY)
   }
 }
